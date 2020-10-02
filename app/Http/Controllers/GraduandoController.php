d4d4349e3481f854f3c637fc8d4507934a111330
RS7 <rsiza@unsa.edu.pe>
2020-10-02 11:26:22 -0500

    modificación graduando para obtener solo de acdiden

--- app/Http/Controllers/GraduandoController.php
+++ app/Http/Controllers/GraduandoController.php
@@ -43,109 +43,109 @@ class GraduandoController extends Controller
 
     public function mover(Request $request)
     {
-      $idruta = $request->idruta;
-      $idexpediente = $request->idexpediente;
-      $idgradproc_origen = $request->idgradproc_origen;
-      $idgradproc_destino = $request->idgradproc_destino;
+        $idruta = $request->idruta;
+        $idexpediente = $request->idexpediente;
+        $idgradproc_origen = $request->idgradproc_origen;
+        $idgradproc_destino = $request->idgradproc_destino;
 
-      try {
+        try {
 
-          DB::beginTransaction();
+            DB::beginTransaction();
 
-          $idusuario = DB::TABLE('gt_usuario')
-                              ->SELECT('id AS idusuario')
-                              ->WHERE('codi_usuario', '=', Auth::user()->cui)
-                              ->FIRST()
-                              ->idusuario;
+            $idusuario = DB::TABLE('gt_usuario')
+                ->SELECT('id AS idusuario')
+                ->WHERE('codi_usuario', '=', Auth::user()->cui)
+                ->FIRST()
+                ->idusuario;
 
-          $mytime= Carbon::now('America/Lima');
+            $mytime = Carbon::now('America/Lima');
 
-          DB::table('gt_movimiento')
-              ->insert([
-              'idexpediente' => $idexpediente,
-              'idusuario' => $idusuario,
-              'fecha' => $mytime->format('Y-m-d H:i:s'),
-              'idruta' => $idruta,
-          ]);
+            DB::table('gt_movimiento')
+                ->insert([
+                    'idexpediente' => $idexpediente,
+                    'idusuario' => $idusuario,
+                    'fecha' => $mytime->format('Y-m-d H:i:s'),
+                    'idruta' => $idruta,
+                ]);
 
-          DB::table('gt_expediente')
-              ->where('id', '=', $idexpediente)
-              ->update(['idgrado_procedimiento' => $idgradproc_destino]);
+            DB::table('gt_expediente')
+                ->where('id', '=', $idexpediente)
+                ->update(['idgrado_procedimiento' => $idgradproc_destino]);
 
-          DB::commit();
-      } catch (Exception $e){
-          DB::rollBack();
-      }
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+        }
     }
 
     public function registrarProyecto(Request $request)
     {
-      $titulo = $request->titulo;
-      $idexpediente = $request->idexpediente;
-      $data = $request->data;
-      $extension = $request->extension;
-      $idruta = $request->idruta;
-      $idgradproc_origen = $request->idgradproc_origen;
-      $idgradproc_destino = $request->idgradproc_destino;
+        $titulo = $request->titulo;
+        $idexpediente = $request->idexpediente;
+        $data = $request->data;
+        $extension = $request->extension;
+        $idruta = $request->idruta;
+        $idgradproc_origen = $request->idgradproc_origen;
+        $idgradproc_destino = $request->idgradproc_destino;
 
-      try {
+        try {
 
-          DB::beginTransaction();
+            DB::beginTransaction();
 
-          $idusuario = DB::TABLE('gt_usuario')
-                              ->SELECT('id AS idusuario')
-                              ->WHERE('codi_usuario', '=', Auth::user()->cui)
-                              ->FIRST()
-                              ->idusuario;
+            $idusuario = DB::TABLE('gt_usuario')
+                ->SELECT('id AS idusuario')
+                ->WHERE('codi_usuario', '=', Auth::user()->cui)
+                ->FIRST()
+                ->idusuario;
 
-          DB::table('gt_expediente')
-              ->where('id', '=', $idexpediente)
-              ->update(['titulo' => $titulo]);
+            DB::table('gt_expediente')
+                ->where('id', '=', $idexpediente)
+                ->update(['titulo' => $titulo]);
 
-          $mytime= Carbon::now('America/Lima');          
+            $mytime = Carbon::now('America/Lima');
 
-          $idmovimiento = DB::table('gt_movimiento')
-                                ->insertGetId([
-                                    'idexpediente' => $idexpediente,
-                                    'idusuario' => $idusuario,
-                                    'fecha' => $mytime->format('Y-m-d H:i:s'),
-                                    'idruta' => $idruta,
-                                    'idmov_anterior' => '0'
-                            ]);
+            $idmovimiento = DB::table('gt_movimiento')
+                ->insertGetId([
+                    'idexpediente' => $idexpediente,
+                    'idusuario' => $idusuario,
+                    'fecha' => $mytime->format('Y-m-d H:i:s'),
+                    'idruta' => $idruta,
+                    'idmov_anterior' => '0'
+                ]);
 
-          DB::table('gt_expediente')
-              ->where('id', '=', $idexpediente)
-              ->update(['idgrado_procedimiento' => $idgradproc_destino]);
+            DB::table('gt_expediente')
+                ->where('id', '=', $idexpediente)
+                ->update(['idgrado_procedimiento' => $idgradproc_destino]);
 
-        
-          $idrecurso = DB::table('gt_recurso')
-                        ->insertGetId([
-                            'idexpediente' => $idexpediente,
-                            'idgrado_proc' => $idgradproc_origen,
-                            'idusuario' => $idusuario,
-                            'idmovimiento' => $idmovimiento,
-                            'idruta' => $idruta           
-                    ]);          
 
-          DB::table('gt_archivo')
-              ->insert([
-                'idrecurso' => $idrecurso,                
-                'nombre_asignado' => 'Plan de tesis',                
-                'nombre_archivo' => 'Plan de tesis.pdf',                
-                'mime' => $extension,     
-                'data' => $data         
-          ]);
+            $idrecurso = DB::table('gt_recurso')
+                ->insertGetId([
+                    'idexpediente' => $idexpediente,
+                    'idgrado_proc' => $idgradproc_origen,
+                    'idusuario' => $idusuario,
+                    'idmovimiento' => $idmovimiento,
+                    'idruta' => $idruta
+                ]);
 
-          //$archivo_pdf = $request->archivo_pdf;
-          /*if (move_uploaded_file($_FILES["file"]["tmp_name"], "pdfs/".$_FILES['file']['name'])) {
+            DB::table('gt_archivo')
+                ->insert([
+                    'idrecurso' => $idrecurso,
+                    'nombre_asignado' => 'Plan de tesis',
+                    'nombre_archivo' => 'Plan de tesis.pdf',
+                    'mime' => $extension,
+                    'data' => $data
+                ]);
+
+            //$archivo_pdf = $request->archivo_pdf;
+            /*if (move_uploaded_file($_FILES["file"]["tmp_name"], "pdfs/".$_FILES['file']['name'])) {
             return "done";
           }*/
 
-          //return $_FILES["file"]["tmp_name"];
-          DB::commit();
-      } catch (Exception $e){
-          DB::rollBack();
-      }
+            //return $_FILES["file"]["tmp_name"];
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+        }
     }
 
     /**
@@ -156,24 +156,30 @@ class GraduandoController extends Controller
      */
     public function show()
     {
-        if (Auth::user()->tipo == 1) {
-            $graduando = User::join('acdiden', 'gt_graduando.cui', '=', 'acdiden.cui')
-                        ->select('gt_graduando.id', 'gt_graduando.cui', DB::raw('(SUBSTRING_INDEX(REPLACE(acdiden.apn, "/", " "), ",", 1)) AS apellidos'),
-                                DB::raw('(SUBSTRING_INDEX(SUBSTRING_INDEX(REPLACE(acdiden.apn, "/", " "), ",", 2), ",", -1)) AS nombres'),
-                                'email', 'acdiden.dic AS dni', 'telefono_fijo', 'telefono_movil','direccion')
-                        ->where('gt_graduando.id', '=', Auth::id())
-                        ->first();
-        }
+        //if (Auth::user()->tipo == 1) {
+        $graduando = User::join('acdiden', 'gt_graduando.cui', '=', 'acdiden.cui')
+            ->select(
+                'gt_graduando.id',
+                'gt_graduando.cui',
+                DB::raw('(SUBSTRING_INDEX(REPLACE(acdiden.apn, "/", " "), ",", 1)) AS apellidos'),
+                DB::raw('(SUBSTRING_INDEX(SUBSTRING_INDEX(REPLACE(acdiden.apn, "/", " "), ",", 2), ",", -1)) AS nombres'),
+                'email',
+                'acdiden.dic AS dni',
+                'telefono_fijo',
+                'telefono_movil',
+                'direccion'
+            )
+            ->where('gt_graduando.id', '=', Auth::id())
+            ->first();
+        /*}
         else if (Auth::user()->tipo == 0) {
             $graduando = User::select('gt_graduando.id', DB::raw('(SUBSTRING_INDEX(gt_graduando.apn, ",", 1)) AS apellidos'),
                                 DB::raw('(SUBSTRING_INDEX(SUBSTRING_INDEX(gt_graduando.apn, ",", 2), ",", -1)) AS nombres'),
                                 'email', 'telefono_fijo', 'telefono_movil','direccion')
                         ->where('gt_graduando.id', '=', Auth::id())
                         ->first();
-        }
-        
-        //dd(json_encode($graduando));
-        
+        }*/
+
         return json_encode($graduando);
     }
 
