b856677ff6099026fa1768154be2449b678b6544
RS7 <rsiza@unsa.edu.pe>
2021-04-13 10:46:59 -0500

    Configuración de webpackmix

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,7 +1,5 @@
-var config = {    
-    //API_URL: '//127.0.0.1:8001',
-    API_URL: '//localhost:3002',
-    //API_URL: 'https://desarrollo.unsa.edu.pe/gtunsa-app/public',
+var config = {        
+    API_URL: process.env.MIX_APP_URL,    
     etiquetas: { actual:"primary", enviar:"success", informar:"success", derivar:"success", devolver:"danger", denegar:"danger",
                  observar:"warning", rechazar:"danger", aceptar:"success", aprobar:"success", }
 }
